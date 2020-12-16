38ed87a5992c9e297341bbe0784bb49af702b6d4
ma061 <963113013@qq,com>
2020-12-16 23:39:29 +0800

    init

--- src/com/company/node/study/day201216/Demo02PhoneOne.java
+++ src/com/company/node/study/day201216/Demo02PhoneOne.java
@@ -5,17 +5,23 @@ public class Demo02PhoneOne {
 //        根据Phone类，创建一个名为one的对象
 //        格式： 类名称 对象名 = new 类名称（）；
         Phone one = new Phone();
-        System.out.println(one.brand); // null
-        System.out.println(one.price); // 0.0
-        System.out.println(one.color); // null
+        // null
+        System.out.println(one.brand);
+        // 0.0
+        System.out.println(one.price);
+        // null
+        System.out.println(one.color);
         System.out.println("======================");
 
         one.brand = "苹果";
         one.price = 8848.0;
         one.color = "金色";
-        System.out.println(one.brand); // 苹果
-        System.out.println(one.price); // 8848.0
-        System.out.println(one.color); // 金色
+        // 苹果
+        System.out.println(one.brand);
+        // 8848.0
+        System.out.println(one.price);
+        // 金色
+        System.out.println(one.color);
         System.out.println("======================");
 
         one.call("金晨");
