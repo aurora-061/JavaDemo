071e99f9ad947e1340fd9c86faf8b3a148bac0fb
Ma963 <963113013@qq.com>
2020-12-26 00:07:58 +0800

    inti

---
+++ src/com/company/node/study/day201226/LeetCodeProblem105.java
@@ -0,0 +1,31 @@
+package com.company.node.study.day201226;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class LeetCodeProblem105 {
+    class Solution {
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        }
+    }
+
+//    递归
+    public TreeNode buildTree(TreeNode root){
+        if(root == null)
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
