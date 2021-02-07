0d698b72cacb05be78ca322c6b7712d1eafaa62c
Ma963 <963113013@qq.com>
2021-02-07 12:52:44 +0800

    inti

--- src/com/company/node/study/vocation/day01/demo02/Person.java
+++ src/com/company/node/study/vocation/day01/demo02/Person.java
@@ -1,4 +1,9 @@
 package com.company.node.study.vocation.day01.demo02;
 
 public class Person {
+    String name;
+
+    public void sayHellow(String name){
+        System.out.println(name + "你好，我是" +this.name);
+    }
 }
